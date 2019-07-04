1115072288f82f659492efc9ae62922222278e58
root <root@jenkins-master.us-central1-a.c.sharp-theory-241513.internal>
2019-07-04 10:33:31 +0000

    nine

--- roles/run_instance/files/script_bash.sh
+++ roles/run_instance/files/script_bash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo apt-get -y install openjdk-8-jdk
+apt-get -y update
+apt-get -y install nginx
+apt-get -y install openjdk-8-jdk
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get -y update
-sudo apt-get -y install jenkins
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get -y update
+apt-get -y install jenkins
